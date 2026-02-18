0beb6e914e7ddb53deb1c751f7cb5243900f4dbc
rushikesh452 <kalerushikesh8701@gmail.com>
2026-02-18 13:44:43 +0530

    Next Updated Programme

--- src/main/java/All_JavaProgrammes/JavaDemo001.java
+++ src/main/java/All_JavaProgrammes/JavaDemo001.java
@@ -1,4 +1,15 @@
 package All_JavaProgrammes;
 
 public class JavaDemo001 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World Java");
+
+        System.out.println("JavaDemo_001");
+
+
+
+
+    }
 }
---
+++ src/main/java/All_JavaProgrammes/JavaDemo002.java
@@ -0,0 +1,11 @@
+package All_JavaProgrammes;
+
+public class JavaDemo002 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Programme Starting ");
+        System.out.println("JavaDemo002");
+    }
+
+}
